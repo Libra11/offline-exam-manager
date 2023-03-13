0b8aadeb7dd08e0f31f8207ae8a4d227de6da692
Libra <libra085925@gmail.com>
2023-03-13 15:01:59 +0800

    eslint prettier

--- electron/preload.js
+++ electron/preload.js
@@ -6,15 +6,15 @@
  * @FilePath: /libra-vue3-all-in-one-template/electron/preload.js
  */
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
+	const replaceText = (selector, text) => {
+		const element = document.getElementById(selector)
 
-    if (element) {
-      element.innerText = text;
-    }
-  };
+		if (element) {
+			element.innerText = text
+		}
+	}
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
-});
+	for (const dependency of ['chrome', 'node', 'electron']) {
+		replaceText(`${dependency}-version`, process.versions[dependency])
+	}
+})
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue';
-import { useDark, useToggle } from '@vueuse/core';
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
+import { onBeforeMount, ref } from 'vue'
+import { useDark, useToggle } from '@vueuse/core'
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 
-const isDark = useDark();
+const isDark = useDark()
 let toggleDark = useToggle(isDark),
-  size = ref<'default' | 'large' | 'small'>('default'),
-  value1 = ref(''),
-  isAnimation = ref(true);
+	size = ref<'default' | 'large' | 'small'>('default'),
+	value1 = ref(''),
+	isAnimation = ref(true)
 
 function removeAnimation() {
-  if (isAnimation.value) return;
-  let style: HTMLStyleElement = document.createElement('style');
-  style.textContent = `
+	if (isAnimation.value) return
+	let style: HTMLStyleElement = document.createElement('style')
+	style.textContent = `
   *, *::before, *::after {
     transition-duration: 0s !important;
     animation-duration: 0s !important;
@@ -22,94 +22,94 @@ function removeAnimation() {
     animation-iteration-count: 1 !important;
     transition-property: none !important;
     animation-name: none !important;
-  }`;
-  document.head.appendChild(style);
+  }`
+	document.head.appendChild(style)
 }
 
 onBeforeMount(() => {
-  removeAnimation();
-});
+	removeAnimation()
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+	<header>
+		<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper no-animation">
-      <HelloWorld msg="You did it!" />
-      <el-button type="primary" @click="toggleDark()">Toggle Dark Mode</el-button>
-      {{ isDark }}
-      <el-date-picker v-model="value1" type="date" placeholder="Pick a day" :size="size" />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+		<div class="wrapper no-animation">
+			<HelloWorld msg="You did it!" />
+			<el-button type="primary" @click="toggleDark()">Toggle Dark Mode</el-button>
+			{{ isDark }}
+			<el-date-picker v-model="value1" type="date" placeholder="Pick a day" :size="size" />
+			<nav>
+				<RouterLink to="/">Home</RouterLink>
+				<RouterLink to="/about">About</RouterLink>
+			</nav>
+		</div>
+	</header>
 
-  <RouterView />
+	<RouterView />
 </template>
 
 <style lang="scss">
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+	line-height: 1.5;
+	max-height: 100vh;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+	display: block;
+	margin: 0 auto 2rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+	width: 100%;
+	font-size: 12px;
+	text-align: center;
+	margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+	color: var(--color-text);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+	background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+	display: inline-block;
+	padding: 0 1rem;
+	border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
-  border: 0;
+	border: 0;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+	header {
+		display: flex;
+		place-items: center;
+		padding-right: calc(var(--section-gap) / 2);
+	}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+	.logo {
+		margin: 0 2rem 0 0;
+	}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+	header .wrapper {
+		display: flex;
+		place-items: flex-start;
+		flex-wrap: wrap;
+	}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+	nav {
+		text-align: left;
+		margin-left: -1rem;
+		font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+		padding: 1rem 0;
+		margin-top: 1rem;
+	}
 }
 </style>
